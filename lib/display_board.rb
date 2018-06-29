90e10736845ec039cb448a81dca9729f4c29a87b
Kari O'Neal <triadfirewood@gmail.com>
2018-06-29 19:28:47 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,7 +3,7 @@
 board = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
 
 def display_board(board)
-  puts "   |   |   "
+  puts " #{board[0], | #   |   "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
