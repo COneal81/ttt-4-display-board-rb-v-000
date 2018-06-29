0fbf05bad7d81815b99b95652fb45f7f5d389758
Kari O'Neal <triadfirewood@gmail.com>
2018-06-29 18:53:50 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+board = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
+
+def display_board(board)
+ 1.times{print"   |   |   "}
+ 1.times{print"\n-----------\n"}
+ 1.times{print"   |   |   "}
+ 1.times{print"\n-----------\n"}
+ 1.times{print"   |   |   \n"}
+end  
